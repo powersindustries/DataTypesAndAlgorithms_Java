15bfdf1bec7c0c323003cd27a8e5e3b1867c43a8
powersindustries <lp.accounts@fastmail.com>
2024-04-29 08:22:28 -0500

    Added a HashTable type with a description in the README.

---
+++ DataTypes/HashTable.java
@@ -0,0 +1,125 @@
+import java.util.ArrayList;
+
+public class HashTable {
+
+    private class Node {
+        private String key;
+        private int value;
+        private Node nextNode;
+
+        public Node(String inKey, int inValue) {
+            this.key = inKey;
+            this.value = inValue;
+        }
+
+    }
+
+    // We set size to 7 because prime numbers will result in less collisions.
+    private int size = 7;
+    private Node[] map;
+
+    public HashTable() {
+        map = new Node[size];
+    }
+
+    public void print() {
+        String output = "";
+
+        for (int x=0; x < map.length; ++x) {
+            Node tempNode = map[x];
+            while(tempNode != null) {
+                output += "{" + tempNode.key + ", " + tempNode.value + "}";
+                tempNode = tempNode.nextNode;
+            }
+        }
+
+        System.out.println(output);
+    }
+
+    // Returns an int that is a hash of the input key string.
+    private int hashMethod(String inKey) {
+        int output = 0;
+
+        char[] keyChar = inKey.toCharArray();
+        for(int x=0; x < keyChar.length; ++x) {
+            int asciiValue = keyChar[x];
+
+            // Using Feel free to use any prime number here.
+            output = (output + asciiValue * 19) % map.length; 
+        }
+
+        return output;
+    }
+
+    // Adds a new data entry to the HashTable.
+    public void set(String inKey, int inValue) {
+        int index = hashMethod(inKey);
+
+        Node newNode = new Node(inKey, inValue);
+        // Case for when index is empty. Set empty index to newNode.
+        if (map[index] == null){
+            map[index] = newNode;
+        }
+        // Case for when index already has a node. Add the newNode to the nextNode of the node at index.
+        // Similar to push() method in linked list.
+        else {
+            Node tempNode = map[index];
+            while (tempNode.nextNode != null) {
+                tempNode = tempNode.nextNode;
+            }
+
+            tempNode.nextNode = newNode;
+        }
+    }
+
+    // Returns the data by key as an Int.
+    public int get(String inKey) {
+        int index = hashMethod(inKey);
+        Node tempNode = map[index];
+        while(tempNode != null) {
+            if (tempNode.key == inKey) {
+                return tempNode.value;
+            }
+
+            tempNode = tempNode.nextNode;
+        }
+
+        return 0;
+    }
+
+    // Returns an ArrayList of all the keys in a hash map.
+    public ArrayList keys() {
+        ArrayList<String> outputKeys = new ArrayList<String>();
+
+        for(int x=0; x< map.length; ++x) {
+            Node tempNode = map[x];
+            while(tempNode != null) {
+                outputKeys.add(tempNode.key);
+                tempNode = tempNode.nextNode;
+            }
+        }
+
+        return outputKeys;
+    }
+
+
+    // -------------------------------------------------
+    // MAIN METHOD
+    // -------------------------------------------------
+    public static void main(String[] args) {
+
+        System.out.println("Hash Table.");
+
+        HashTable hashTable = new HashTable();
+
+        hashTable.set("test1", 1);
+        hashTable.set("test2", 5);
+        hashTable.set("test3", 10);
+
+        hashTable.print();
+    
+        System.out.println(hashTable.keys());
+
+    }
+}
+
--- README.md
+++ README.md
@@ -26,8 +26,8 @@
 
 
 ### Binary Search Tree
-- Similar to a Linked List except that it branches in multiple directions.
-- Binary Search Tree nodes branch left and right.
+- BSTs are similar to a Linked List except that it branches in multiple directions.
+- BST nodes branch left and right.
 - Nodes on the left are less than nodes on the right.
 - Tree Definitions
     - `Full Tree` -> Each node points to 0 or 2 nodes.
@@ -35,3 +35,16 @@
     - `Complete Tree` -> The furthest reaching row in the tree is filled from left to right.
 - Big O Information:
     - O(logN) -> Uses Divide and Conquer to achieve this. Most nodes get completely ignored.
+
+
+### Hash Tables
+- Hash Tables are a data container where each piece of data is searchable through a {Key, Value} pair.
+- How It works:
+    - Array with a prime number amount of entries.
+    - When adding a new entry, we take the key and hash it. This Hash value becomes the index in the array.
+        - `Hash Method` -> Takes a string and turns it into a number. Calculation is 1 way, you can turn the key to a hash but the hashed index cant turn back into the original key.
+    - Each entry in the array is a linked list. When we have multiple entries with say key, the duplicate key values get added to the end of the linked list.
+- Big O Information:
+    - Hash Method -> O(1)
+    - Set Method -> O(1)
+    - Get Method -> O(1)
